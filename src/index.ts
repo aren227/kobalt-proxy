fe9822ffd91abea6e5bbb182dc66662f1db7a5c9
aren227 <mathdong01@gmail.com>
2021-10-26 13:36:41 +0900

    Use express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { createServer } from 'http';
+import express from 'express';
+import cors from 'cors';
 import AmpqClient from './amqpClient';
 import { createProxyServer } from 'http-proxy';
 import { SessionMap } from './sessionMap';
@@ -10,78 +11,54 @@ const proxy = createProxyServer({ ws: true });
 
 const sessionMap = new SessionMap();
 
-createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+const app = express();
 
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200);
-    res.end();
+app.use(cors());
+
+app.post('/compile', (req, res) => {
+  const msg = req.body;
+  console.log(msg);
+  if (!msg?.language || !msg?.code) {
+    res.status(400).json({ result: 'invalid_request' });
     return;
   }
 
-  if (req.url === '/compile' && req.method === 'POST') {
-    let data = '';
+  amqpClient.requestCompile(
+    {
+      language: msg.language as string,
+      code: msg.code as string,
+    },
+    (result) => {
+      console.log(result);
 
-    req.on('data', (chunk) => {
-      data += chunk;
-    });
+      let response = {} as CompileResultApiResponse;
 
-    req.on('end', () => {
-      try {
-        const msg = JSON.parse(data);
-        if (!msg?.language || !msg?.code) throw Error('Invalid request');
+      if (result.result === 'success' && result.session_id && result.address) {
+        sessionMap.setAddress(result.session_id, result.address);
 
-        amqpClient.requestCompile(
-          {
-            language: msg.language as string,
-            code: msg.code as string,
-          },
-          (result) => {
-            console.log(result);
-
-            let response = {} as CompileResultApiResponse;
-
-            if (
-              result.result === 'success' &&
-              result.session_id &&
-              result.address
-            ) {
-              sessionMap.setAddress(result.session_id, result.address);
-
-              response.result = 'success';
-              response.session_id = result.session_id;
-            } else {
-              response.result = result.result;
-            }
-
-            res.writeHead(200, {
-              'Content-Type': 'application/json',
-            });
-            res.write(JSON.stringify(response));
-            res.end();
-          }
-        );
-      } catch {
-        res.writeHead(400).end();
+        response.result = 'success';
+        response.session_id = result.session_id;
+      } else {
+        response.result = result.result;
       }
-    });
-  } else {
-    res.writeHead(404);
-    res.end();
-  }
-})
-  .on('upgrade', (req, socket, head) => {
-    if (req.url && req.url.startsWith('/') && req.url.length > 1) {
-      const sessionId = req.url.substr(1);
 
-      const address = sessionMap.getAddress(sessionId);
-
-      if (address) {
-        proxy.ws(req, socket, head, { target: address });
-
-        sessionMap.removeAddress(sessionId);
-      }
+      res.json(response);
     }
-  })
-  .listen(8080);
+  );
+});
+
+const server = app.listen(8080);
+
+server.on('upgrade', (req, socket, head) => {
+  if (req.url && req.url.startsWith('/') && req.url.length > 1) {
+    const sessionId = req.url.substr(1);
+
+    const address = sessionMap.getAddress(sessionId);
+
+    if (address) {
+      proxy.ws(req, socket, head, { target: address });
+
+      sessionMap.removeAddress(sessionId);
+    }
+  }
+});
